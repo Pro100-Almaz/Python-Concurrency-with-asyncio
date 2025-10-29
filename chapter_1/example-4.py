beb37eda03fd70d5dc491b9efc58422ebb9d3f25
fletcher <almaz.a@investlink.io>
2025-10-29 13:02:26 +0500

    added 4th image

--- chapter_1/example-4.py
+++ chapter_1/example-4.py
@@ -14,10 +14,28 @@ def fib_no_threading():
     print_fib(35)
     print_fib(36)
 
+def fib_with_threading():
+    thread_1 = threading.Thread(target=print_fib, args=(35,))
+    thread_2 = threading.Thread(target=print_fib, args=(36,))
+
+    thread_1.start()
+    thread_2.start()
+
+    thread_1.join()
+    thread_2.join()
+
 start_time = time.time()
 
 fib_no_threading()
 
 end_time = time.time()
 
-print(f'Время работы {end_time - start_time:.4f} c.')
+print(f'Время работы для fib_no_threading {end_time - start_time:.4f} c.')
+
+start_time = time.time()
+
+fib_with_threading()
+
+end_time = time.time()
+
+print(f'Время работы для fib_with_threading {end_time - start_time:.4f} c.')
